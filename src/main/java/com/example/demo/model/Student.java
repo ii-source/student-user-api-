04c24d93f72b59b9562071db0c31f66330956203
Imam Hossain <imammithu123@gmail.com>
2026-06-02 22:07:22 +0600

    added setter getter fir age

--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -22,4 +22,7 @@ public class Student {
 
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
+
+    public Integer getAge() { return age; }
+    public void setAge(Integer age) { this.age = age; }
 }
